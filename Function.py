ad76d48e1c79862e597d636d5502cd51b1942332
Ankit Sharma <ankitsharma60784@gmail.com>
2025-10-13 20:12:51 +0530

    Create Function.py

---
+++ Function.py
@@ -0,0 +1,29 @@
+# def get_otp():     #def ek magical words hai Jaise java me void hota hai..
+#     otp=2344
+#     print("Ypur otp is:",otp)
+# get_otp() #function calling
+
+#using print(f"")
+
+# def get_otp():#def ek magical words hai kaise java me void hota hai
+#     otp=2344
+#     print("Ypur otp is:",otp)
+# get_otp()
+
+##Otp using random libray
+# from random import randint
+# def get_otp():#def ek magical words hai kaise java me void hota hai
+#     otp=randint(1000,10000)
+#     print(f"Your otp is:{otp}")
+# get_otp()
+
+
+##passing parameters to function
+# def get_otp(data):#def ek magical words hai kaise java me void hota hai
+#     print(f"Your otp is:{data} you can enter it on website")
+# get_otp(2311)
+
+##Passing two parameters in function
+# def get_otp(data,name):
+#     print(f"Hi {name},Your otp is:{data} you can enter it on website")
+# get_otp(2311,"Ankit")
